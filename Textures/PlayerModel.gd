3a712f9c884b44eee10ba77b5d39085da16f258c
noahucs <132694964+noahucs@users.noreply.github.com>
2023-06-02 11:21:25 -0600

    movement finished

--- Textures/PlayerModel.gd
+++ Textures/PlayerModel.gd
@@ -1,46 +1,48 @@
 extends KinematicBody2D
 
-
 const DIRECTION_TO_FRAME := {
 	Vector2.DOWN: 0,
 	Vector2.RIGHT: 2,
 	Vector2.UP: 4,
 }
 
-
-#const DIRECTION_TO_FRAME := {
-#	Vector2.DOWN: 0,
-#	Vector2.DOWN + Vector2.RIGHT: 1,
-#	Vector2.RIGHT: 2,
-#	Vector2.UP + Vector2.RIGHT: 3,
-#	Vector2.UP: 4,
-#}
-
-
-var drag_factor := 0.13
+onready var sprite := $Sprite
+export var movement_speed := 150.0
 var velocity := Vector2.ZERO
 
-onready var sprite := $Sprite
-const Vf := 100.0
-const Vi := 0.0
-const Gravity := 150.0
+export var jump_height : float = 50.0
+export var jump_max_height : float = 0.5
+export var jump_min_height : float = 0.4
 
-const SPEED := 390.0
+onready var jump_speed : float = (2.0 * jump_height) / jump_max_height * -1.0
+onready var jump_gravity : float = (-2.0 * jump_height) / (jump_max_height * jump_max_height) * -1.0
+onready var jump_fall_gravity : float = (-2.0 * jump_height) / (jump_min_height * jump_min_height) * -1.0
+
 func _physics_process(delta: float) -> void:
-	#Basic Movement
-	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	var gravity := delta * Gravity
-	var desired_velocity := SPEED * direction
-	var steering_vector := desired_velocity - velocity
-	velocity += steering_vector * drag_factor
-	velocity = move_and_slide(velocity)
-	var direction_key := direction.round()
-	direction_key.x = abs(direction_key.x)
+	velocity.y += get_gravity() * delta
+	velocity.x = get_input_velocity() * movement_speed
+	if Input.is_action_just_pressed("move_jump") and is_on_floor():
+		jump()
+		
+	velocity + move_and_slide(velocity, Vector2.UP)
 	
-	#flip for model
-	if direction_key in DIRECTION_TO_FRAME:
-		sprite.frame = DIRECTION_TO_FRAME[direction_key]
-		sprite.flip_h = sign(direction.x) == -1
-	#Physics for moving up.
+func get_gravity() -> float:
+	return jump_gravity if velocity.y < 0.0 else jump_fall_gravity
+	
+func jump():
+	velocity.y = jump_speed
+	
+	
+func get_input_velocity() -> float:
+	var horizontal := 0.0
+	
+	if Input.is_action_pressed("move_left"):
+		sprite.frame = DIRECTION_TO_FRAME[Vector2.RIGHT]
+		sprite.flip_h = sign(horizontal) == -1
+		horizontal -= 1.0
+	if Input.is_action_pressed("move_right"):
+		horizontal += 1.0
+	
+	return horizontal
 	
 	
